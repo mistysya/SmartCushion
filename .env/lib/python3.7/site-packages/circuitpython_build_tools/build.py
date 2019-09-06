1935a9db477c49622da6b4eb2b0acba23cba9a45
DaDa0413 <actf5795nb8@gmail.com>
2019-09-06 17:14:14 +0800

    Add virtual environment

---
+++ .env/lib/python3.7/site-packages/circuitpython_build_tools/build.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python3
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 Scott Shawcroft for Adafruit Industries
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import os
+import os.path
+import semver
+import shutil
+import sys
+import subprocess
+import tempfile
+
+IGNORE_PY = ["setup.py", "conf.py", "__init__.py"]
+
+def version_string(path=None, *, valid_semver=False):
+    version = None
+    tag = subprocess.run('git describe --tags --exact-match', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=path)
+    if tag.returncode == 0:
+        version = tag.stdout.strip().decode("utf-8", "strict")
+    else:
+        describe = subprocess.run("git describe --tags --always", shell=True, stdout=subprocess.PIPE, cwd=path)
+        describe = describe.stdout.strip().decode("utf-8", "strict").rsplit("-", maxsplit=2)
+        if len(describe) == 3:
+            tag, additional_commits, commitish = describe
+            commitish = commitish[1:]
+        else:
+            tag = "0.0.0"
+            commit_count = subprocess.run("git rev-list --count HEAD", shell=True, stdout=subprocess.PIPE, cwd=path)
+            additional_commits = commit_count.stdout.strip().decode("utf-8", "strict")
+            commitish = describe[0]
+        if valid_semver:
+            version_info = semver.parse_version_info(tag)
+            if not version_info.prerelease:
+                version = semver.bump_patch(tag) + "-alpha.0.plus." + additional_commits + "+" + commitish
+            else:
+                version = tag + ".plus." + additional_commits + "+" + commitish
+        else:
+            version = commitish
+    return version
+
+def mpy_cross(mpy_cross_filename, circuitpython_tag, quiet=False):
+    if os.path.isfile(mpy_cross_filename):
+        return
+    if not quiet:
+        title = "Building mpy-cross for circuitpython " + circuitpython_tag
+        print()
+        print(title)
+        print("=" * len(title))
+
+    os.makedirs("build_deps/", exist_ok=True)
+    if not os.path.isdir("build_deps/circuitpython"):
+        clone = subprocess.run("git clone https://github.com/adafruit/circuitpython.git build_deps/circuitpython", shell=True)
+        if clone.returncode != 0:
+            sys.exit(clone.returncode)
+
+    current_dir = os.getcwd()
+    os.chdir("build_deps/circuitpython")
+    make = subprocess.run("git fetch && git checkout {TAG} && git submodule update".format(TAG=circuitpython_tag), shell=True)
+    os.chdir("tools")
+    make = subprocess.run("git submodule update --init .", shell=True)
+    os.chdir("../mpy-cross")
+    make = subprocess.run("make clean && make", shell=True)
+    os.chdir(current_dir)
+
+    shutil.copy("build_deps/circuitpython/mpy-cross/mpy-cross", mpy_cross_filename)
+
+    if make.returncode != 0:
+        sys.exit(make.returncode)
+
+def _munge_to_temp(original_path, temp_file, library_version):
+    with open(original_path, "rb") as original_file:
+        for line in original_file:
+            if original_path.endswith(".bin"):
+                # this is solely for adafruit_framebuf/examples/font5x8.bin
+                temp_file.write(line)
+            else:
+                line = line.decode("utf-8").strip("\n")
+                if line.startswith("__version__"):
+                    line = line.replace("0.0.0-auto.0", library_version)
+                temp_file.write(line.encode("utf-8") + b"\r\n")
+    temp_file.flush()
+
+def library(library_path, output_directory, package_folder_prefix,
+            mpy_cross=None, example_bundle=False):
+    py_files = []
+    package_files = []
+    example_files = []
+    total_size = 512
+    for filename in os.listdir(library_path):
+        full_path = os.path.join(library_path, filename)
+        if os.path.isdir(full_path):
+            path_walk = [names for names in os.walk(full_path)]
+            #print("- '{}' walk: {}".format(filename, path_walk))
+
+            # iterate through path_walk, appending each file to
+            # 'walked_files' while retaining subdirectory structure
+            walked_files = []
+            for path in path_walk:
+                rel_path = ""
+                if filename.startswith("examples"):
+                    path_tail_idx = path[0].rfind("examples/")
+                    if path_tail_idx != -1:
+                        rel_path = "{}/".format(path[0][path_tail_idx + 9:])
+
+                else:
+                    path_tail_idx = (path[0].rfind("{}/".format(filename))
+                                     + (len(filename) +1))
+                    path_tail = path[0][path_tail_idx:]
+
+                    # if this entry is the package top dir, keep it
+                    # empty so we don't double append the dir name
+                    if filename not in path_tail:
+                        rel_path = "{}/".format(path_tail)
+
+                for path_files in path[2]:
+                    walked_files.append("{}{}".format(rel_path, path_files))
+            #print(" - expanded file walk: {}".format(walked_files))
+
+            files = filter(
+                lambda x: x.endswith(".py") or x.startswith("font5x8.bin"),
+                walked_files
+            )
+            files = map(lambda x: os.path.join(filename, x), files)
+
+            if filename.startswith("examples"):
+                example_files.extend(files)
+                #print("- example files: {}".format(example_files))
+            else:
+                if (not example_bundle and
+                    not filename.startswith(package_folder_prefix)):
+                        print("skipped path: {}".format(full_path))
+                        continue
+                if not example_bundle:
+                    package_files.extend(files)
+                    #print("- package files: {} | {}".format(filename, package_files))
+
+        if (filename.endswith(".py") and
+            filename not in IGNORE_PY and
+            not example_bundle):
+                py_files.append(filename)
+
+    if len(py_files) > 1:
+        raise ValueError("Multiple top level py files not allowed. Please put them in a package "
+                         "or combine them into a single file.")
+
+    for fn in example_files:
+        base_dir = os.path.join(output_directory.replace("/lib", "/"), os.path.dirname(fn))
+        if not os.path.isdir(base_dir):
+            os.makedirs(base_dir)
+            total_size += 512
+
+    for fn in package_files:
+        base_dir = os.path.join(output_directory, os.path.dirname(fn))
+        if not os.path.isdir(base_dir):
+            os.makedirs(base_dir)
+            total_size += 512
+
+    new_extension = ".py"
+    if mpy_cross:
+        new_extension = ".mpy"
+
+    try:
+        library_version = version_string(library_path, valid_semver=True)
+    except ValueError as e:
+        print(library_path + " has version that doesn't follow SemVer (semver.org)")
+        print(e)
+        library_version = version_string(library_path)
+
+    for filename in py_files:
+        full_path = os.path.join(library_path, filename)
+        output_file = os.path.join(output_directory,
+                                   filename.replace(".py", new_extension))
+        with tempfile.NamedTemporaryFile() as temp_file:
+            _munge_to_temp(full_path, temp_file, library_version)
+
+            if mpy_cross:
+                mpy_success = subprocess.call([mpy_cross,
+                                               "-o", output_file,
+                                               "-s", filename,
+                                               temp_file.name])
+                if mpy_success != 0:
+                    raise RuntimeError("mpy-cross failed on", full_path)
+            else:
+                shutil.copyfile(temp_file.name, output_file)
+
+    for filename in package_files:
+        full_path = os.path.join(library_path, filename)
+        with tempfile.NamedTemporaryFile() as temp_file:
+            _munge_to_temp(full_path, temp_file, library_version)
+            if not mpy_cross or os.stat(full_path).st_size == 0:
+                output_file = os.path.join(output_directory, filename)
+                shutil.copyfile(temp_file.name, output_file)
+            else:
+                output_file = os.path.join(output_directory,
+                                           filename.replace(".py", new_extension))
+                mpy_success = subprocess.call([mpy_cross,
+                                               "-o", output_file,
+                                               "-s", filename,
+                                               temp_file.name])
+                if mpy_success != 0:
+                    raise RuntimeError("mpy-cross failed on", full_path)
+
+    for filename in example_files:
+        full_path = os.path.join(library_path, filename)
+        output_file = os.path.join(output_directory.replace("/lib", "/"), filename)
+        with tempfile.NamedTemporaryFile() as temp_file:
+            _munge_to_temp(full_path, temp_file, library_version)
+            shutil.copyfile(temp_file.name, output_file)
---
+++ .env/lib/python3.7/site-packages/circuitpython_build_tools/scripts/build_bundles.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2016-2017 Scott Shawcroft for Adafruit Industries
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import json
+import os
+import os.path
+import shlex
+import shutil
+import subprocess
+import sys
+import zipfile
+
+import click
+
+from circuitpython_build_tools import build
+from circuitpython_build_tools import target_versions
+
+import pkg_resources
+
+def add_file(bundle, src_file, zip_name):
+    bundle.write(src_file, zip_name)
+    file_size = os.stat(src_file).st_size
+    file_sector_size = file_size
+    if file_size % 512 != 0:
+        file_sector_size = (file_size // 512 + 1) * 512
+    print(zip_name, file_size, file_sector_size)
+    return file_sector_size
+
+
+def build_bundle(libs, bundle_version, output_filename, package_folder_prefix,
+        build_tools_version="devel", mpy_cross=None, example_bundle=False):
+    build_dir = "build-" + os.path.basename(output_filename)
+    top_folder = os.path.basename(output_filename).replace(".zip", "")
+    build_lib_dir = os.path.join(build_dir, top_folder, "lib")
+    build_example_dir = os.path.join(build_dir, top_folder, "examples")
+    if os.path.isdir(build_dir):
+        print("Deleting existing build.")
+        shutil.rmtree(build_dir)
+    total_size = 0
+    if not example_bundle:
+        os.makedirs(build_lib_dir)
+        total_size += 512
+    os.makedirs(build_example_dir)
+    total_size += 512
+
+    multiple_libs = len(libs) > 1
+
+    success = True
+    for library_path in libs:
+        try:
+            build.library(library_path, build_lib_dir,  package_folder_prefix,
+                          mpy_cross=mpy_cross, example_bundle=example_bundle)
+        except ValueError as e:
+            print("build.library failure:", library_path)
+            print(e)
+            success = False
+
+    print()
+    print("Generating VERSIONS")
+    if multiple_libs:
+        with open(os.path.join(build_dir, top_folder, "VERSIONS.txt"), "w") as f:
+            f.write(bundle_version + "\r\n")
+            versions = subprocess.run('git submodule --quiet foreach \"git remote get-url origin && git describe --tags\"', shell=True, stdout=subprocess.PIPE, cwd=os.path.commonpath(libs))
+            if versions.returncode != 0:
+                print("Failed to generate versions file. Its likely a library hasn't been "
+                      "released yet.")
+                success = False
+
+            repo = None
+            for line in versions.stdout.split(b"\n"):
+                if not line:
+                    continue
+                if line.startswith(b"git@"):
+                    repo = b"https://github.com/" + line.split(b":")[1][:-len(".git")]
+                elif line.startswith(b"https:"):
+                    repo = line.strip()[:-len(".git")]
+                else:
+                    f.write(repo.decode("utf-8", "strict") + "/releases/tag/" + line.strip().decode("utf-8", "strict") + "\r\n")
+
+    if not success:
+        print("WARNING: some failures above")
+        sys.exit(2)
+
+    print()
+    print("Zipping")
+
+    with zipfile.ZipFile(output_filename, 'w') as bundle:
+        build_metadata = {"build-tools-version": build_tools_version}
+        bundle.comment = json.dumps(build_metadata).encode("utf-8")
+        if multiple_libs:
+            total_size += add_file(bundle, "README.txt", os.path.join(top_folder, "README.txt"))
+        for root, dirs, files in os.walk(build_dir):
+            ziproot = root[len(build_dir + "/"):]
+            for filename in files:
+                total_size += add_file(bundle, os.path.join(root, filename),
+                                       os.path.join(ziproot, filename.replace("-", "_")))
+
+    print()
+    print(total_size, "B", total_size / 1024, "kiB", total_size / 1024 / 1024, "MiB")
+    print("Bundled in", output_filename)
+
+def _find_libraries(current_path, depth):
+    if depth <= 0:
+        return [current_path]
+    subdirectories = []
+    for subdirectory in os.listdir(current_path):
+        path = os.path.join(current_path, subdirectory)
+        if os.path.isdir(path):
+            subdirectories.extend(_find_libraries(path, depth - 1))
+    return subdirectories
+
+@click.command()
+@click.option('--filename_prefix', required=True, help="Filename prefix for the output zip files.")
+@click.option('--output_directory', default="bundles", help="Output location for the zip files.")
+@click.option('--library_location', required=True, help="Location of libraries to bundle.")
+@click.option('--library_depth', default=0, help="Depth of library folders. This is useful when multiple libraries are bundled together but are initially in separate subfolders.")
+@click.option('--package_folder_prefix', default="adafruit_", help="Prefix string used to determine package folders to bundle.")
+def build_bundles(filename_prefix, output_directory, library_location, library_depth, package_folder_prefix):
+    os.makedirs(output_directory, exist_ok=True)
+
+    bundle_version = build.version_string()
+
+    libs = _find_libraries(os.path.abspath(library_location), library_depth)
+
+    pkg = pkg_resources.get_distribution("circuitpython-build-tools")
+    build_tools_version = "devel"
+    if pkg:
+        build_tools_version = pkg.version
+
+    build_tools_fn = "z-build_tools_version-{}.ignore".format(
+        build_tools_version)
+    build_tools_fn = os.path.join(output_directory, build_tools_fn)
+    with open(build_tools_fn, "w") as f:
+        f.write(build_tools_version)
+
+    # Build raw source .py bundle
+    zip_filename = os.path.join(output_directory,
+        filename_prefix + '-py-{VERSION}.zip'.format(
+            VERSION=bundle_version))
+    build_bundle(libs, bundle_version, zip_filename, package_folder_prefix,
+                 build_tools_version=build_tools_version)
+
+    # Build .mpy bundle(s)
+    os.makedirs("build_deps", exist_ok=True)
+    for version in target_versions.VERSIONS:
+        # Use prebuilt mpy-cross on Travis, otherwise build our own.
+        if "TRAVIS" in os.environ:
+            mpy_cross = pkg_resources.resource_filename(
+                target_versions.__name__, "data/mpy-cross-" + version["name"])
+        else:
+            mpy_cross = "build_deps/mpy-cross-" + version["name"]
+            build.mpy_cross(mpy_cross, version["tag"])
+        zip_filename = os.path.join(output_directory,
+            filename_prefix + '-{TAG}-mpy-{VERSION}.zip'.format(
+                TAG=version["name"],
+                VERSION=bundle_version))
+        build_bundle(libs, bundle_version, zip_filename, package_folder_prefix,
+                     mpy_cross=mpy_cross, build_tools_version=build_tools_version)
+
+    # Build example bundle
+    zip_filename = os.path.join(output_directory,
+        filename_prefix + '-examples-{VERSION}.zip'.format(
+            VERSION=bundle_version))
+    build_bundle(libs, bundle_version, zip_filename, package_folder_prefix,
+                 build_tools_version=build_tools_version, example_bundle=True)
